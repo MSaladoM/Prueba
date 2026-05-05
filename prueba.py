f158dbaf08dddb459676f6badf8d0e381f7e770c
Tonatiuh Hernández-del-Toro <tonahdztoro@gmail.com>
2026-05-04 20:54:40 -0600

    borre parte de graficas

--- prueba.py
+++ prueba.py
@@ -10,19 +10,6 @@ print(df.head(), "\n")
 print(df.info(), "\n")
 print(df.describe(), "\n")
 print(df['species'].value_counts(), "\n")
-print(df['sepal_width'].mean)
+print(df['sepal_width'].mean())
 
-
-# Visualizaciones
-sns.pairplot(df, hue='species', diag_kind='kde')
-plt.show()
-
-plt.figure(figsize=(10,6))
-sns.boxplot(data=df.drop('species', axis=1))
-plt.title('Distribución de características')
-plt.show()
-
-plt.figure(figsize=(8,5))
-sns.heatmap(df.drop('species', axis=1).corr(), annot=True, cmap='coolwarm')
-plt.title('Matriz de correlación')
-plt.show()
+print('Soy bien chida!!!!!')
